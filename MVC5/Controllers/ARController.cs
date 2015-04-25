b00f6c11df7f313a65ffcbca50cfc2c38422cf3a
ynomearod <ynomearod@gmail.com>
2015-04-25 17:30:58 +0800

    練習 ViewResult 與 ContentResult

---
+++ MVC5/Controllers/ARController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5.Controllers
+{
+    public class ARController : Controller
+    {
+        // GET: AR
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult View1()
+        {
+            return View("View2");
+        }
+
+        public ActionResult View2()
+        {
+            return PartialView();
+        }
+
+        public ActionResult ContentView()
+        {
+            return Content("<svcType>Balinq</svcType>", "text/xml");
+        }
+    }
+}
