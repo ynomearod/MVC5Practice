4bddeb039f3af914a2b384b5dfc6cfd167b4bd26
ynomearod <ynomearod@gmail.com>
2015-05-10 09:07:34 +0800

    練習

--- MVC5/Models/Client.Partial.cs
+++ MVC5/Models/Client.Partial.cs
@@ -26,6 +26,7 @@ namespace MVC5.Models
         
         [StringLength(1, ErrorMessage="欄位長度不得大於 1 個字元")]
         public string Gender { get; set; }
+        
         public Nullable<System.DateTime> DateOfBirth { get; set; }
         public Nullable<double> CreditRating { get; set; }
         
