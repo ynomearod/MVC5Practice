5dcd191cb29e0eb38a6a1039e26bc9569ddeca16
ynomearod <ynomearod@gmail.com>
2015-05-10 10:13:49 +0800

    練習使用 @Ajax.ActionLink 修改 /Clients/Index 頁面的 Details 連結，讓 Details 的內容動態載入到該頁下方

--- MVC5/App_Start/BundleConfig.cs
+++ MVC5/App_Start/BundleConfig.cs
@@ -14,6 +14,10 @@ namespace MVC5
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                       "~/Scripts/jquery-1.11.2.min.js",
+                       "~/Scripts/jquery.unobtrusive-ajax.js"));
+
             // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
             // 準備好實際執行時，請使用 http://modernizr.com 上的建置工具，只選擇您需要的測試。
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ MVC5/Controllers/AjaxController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5.Controllers
+{
+    public class AjaxController : Controller
+    {
+        // GET: Ajax
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string GetTime()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
